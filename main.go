d6ec72ee0ebb7a8815a7bb494897c59147c11073
Salomon Chambi <salomonchambi@gmail.com>
2024-04-13 09:00:00 -0500

    feat: serve a different file type (image)

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	const port = "8080"
 	const filepathRoot = "."
+	// const filepathAssets = "./assets" // this along with 'http.StripePrefix' are no necesary!
 
 	// create a new ServeMux (HTTP request multiplexer) to route incoming requests
 	mux := http.NewServeMux()
@@ -15,34 +16,23 @@ func main() {
 	// build and run fileserver that serves `index.html` file from root at port 8080
 	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 
+	// serve files from the assets directory
+	// mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir(filepathAssets))))
+
 	// create and Initialize http.Server Struct (as Pointer)
 	// memory efficiency: Only the pointer (which is small) is passed around,
 	// not a copy of the entire struct instance
 	server := &http.Server{
-		Addr: "localhost:" + port, // // Binds only to localhost
+		// Addr:    ":" + port, // Binds to all interfaces
+		Addr: "localhost:" + port, // Binds only to localhost
 		Handler: mux, // this tells the server to route incoming requests using the ServeMux
 	}
 
-	log.Printf("...⚡ Serving files from %s on port: %v\n", filepathRoot, port)
+	log.Printf("...⚡ Serving files from %s on port: %s\n", filepathRoot, port)
+	// log.Printf("...⚡ Serving on port: %s\n", port)
 	log.Println("...💀 Close the server with `Ctrl + C`")
 
 	// start the server, use 'ListenAndServe' method,
 	// log.Fatal: logs the error returned by ListenAndServe and exits the program immediately
 	log.Fatal(server.ListenAndServe())
 }
-
-/* 
-func main() {
-	const port = "8080"
-
-	mux := http.NewServeMux()
-
-	srv := &http.Server{
-		Addr:    ":" + port, // // Binds to all interfaces
-		Handler: mux,
-	}
-
-	log.Printf("Serving on port: %s\n", port)
-	log.Fatal(srv.ListenAndServe())
-}
- */
