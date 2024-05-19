e18ee074ac87647427c47ff5714bc7254f8927d2
Salomon Chambi <salomonchambi@gmail.com>
2024-05-19 09:00:00 -0500

    feat: add healthz endpoint and update fileserver path

--- main.go
+++ main.go
@@ -9,8 +9,15 @@ func main() {
 	const port = "8080"
 	const filepathRoot = "."
 
+	// create a new ServeMux (HTTP request multiplexer) to route incoming requests
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
+	// serve files from the root directory under the /app/* path
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+
+	// register the /healthz endpoint
+	mux.HandleFunc("/healthz", healthCheckHandler)
+
 
 	server := &http.Server{
 		// Addr:    ":" + port, // Binds to all interfaces
@@ -20,5 +27,13 @@ func main() {
 
 	log.Printf("...⚡ Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Println("...💀 Stop the server using `Ctrl + C`")
+
 	log.Fatal(server.ListenAndServe())
+}
+
+// Handler function for /healthz
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("OK"))
 }
