99b903c651bd74deb8827307130d19271ff25a5a
Salomon Chambi <salomonchambi@gmail.com>
2024-05-21 09:00:00 -0500

    feat: add custom handlers and a middlreware

--- main.go
+++ main.go
@@ -3,7 +3,36 @@ package main
 import (
 	"log"
 	"net/http"
+	"fmt"
 )
+
+// struct to hold any stateful (in-memory data)
+type apiConfig struct {
+	fileserverHits int
+}
+
+// middleware method to increment hits and call next handler
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits +=1
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hits: %d\n", cfg.fileserverHits)
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+}
+
+// Handler function for /healthz
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
 
 func main() {
 	const port = "8080"
@@ -12,16 +41,22 @@ func main() {
 	// create a new ServeMux (HTTP request multiplexer) to route incoming requests
 	mux := http.NewServeMux()
 
-	// serve files from the root directory under the /app/* path
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	// intanciate from struct
+	apiCfg := &apiConfig{}
 
-	// register the /healthz endpoint
-	mux.HandleFunc("/healthz", handleReadiness)
+	// use a variable to avoid long line
+	fileServerHandler := http.FileServer(http.Dir(filepathRoot))
+	// serve files from the root directory under the /app/* path, wrap fileserver with middleware
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(fileServerHandler)))
 
+	// endpoint registers
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	server := &http.Server{
-		// Addr:    ":" + port, // Binds to all interfaces
-		Addr: "localhost:" + port, // Binds only to localhost
+		// Addr:    ":" + port, // binds to all interfaces
+		Addr: "localhost:" + port, // binds only to localhost
 		Handler: mux, // this tells the server to route incoming requests using the ServeMux
 	}
 
@@ -29,11 +64,4 @@ func main() {
 	log.Println("...💀 Stop the server using `Ctrl + C`")
 
 	log.Fatal(server.ListenAndServe())
-}
-
-// Handler function for /healthz
-func handleReadiness(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte("OK"))
 }
