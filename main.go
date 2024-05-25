6cf96bdf33571aa1c95c0ba02c992f8e62d03a19
Salomon Chambi <salomonchambi@gmail.com>
2024-05-25 09:00:00 -0500

    refactor: apply separation of concerns principle

--- main.go
+++ main.go
@@ -3,45 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
-	"fmt"
 )
 
 // struct to hold any stateful (in-memory data)
 type apiConfig struct {
 	fileserverHits int
-}
-
-// middleware method to increment hits and call next handler
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits +=1
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprintf(w, "Hits: %d\n", cfg.fileserverHits)
-	htmlTemplate := `
-    <html>
-    <body>
-        <h1>Welcome, Chirpy Admin</h1>
-        <p>Chirpy has been visited %d times!</p>
-    </body>
-    </html>	
-	`
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, htmlTemplate, cfg.fileserverHits)
-}
-
-func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits = 0
-}
-
-// Handler function for /healthz
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK\n"))
 }
 
 func main() {
