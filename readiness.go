59c669ff4885fc19bc071945ce0fb124520b1cf2
Salomon Chambi <salomonchambi@gmail.com>
2024-05-26 15:23:09 -0500

    feat: create handler for healthz endpoint

--- readiness.go
+++ readiness.go
@@ -8,5 +8,6 @@ import (
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK\n"))
+	// w.Write([]byte(http.StatusText(http.StatusOK)))
+	w.Write([]byte("OK\n")) // simpler and better option than line above
 }
