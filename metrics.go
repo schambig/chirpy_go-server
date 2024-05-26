87d466e6c6621155c9dd3ec98aab0b8017eacaf5
Salomon Chambi <salomonchambi@gmail.com>
2024-05-26 15:20:49 -0500

    refactor: add mutex locks to prevent race conditions

--- metrics.go
+++ metrics.go
@@ -8,21 +8,27 @@ import (
 // middleware method to increment hits and call next handler
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits +=1
+		cfg.mu.Lock() // lock for writing
+		defer cfg.mu.Unlock() // ensure unlock after writing
+
+		cfg.fileserverHits += 1
 		next.ServeHTTP(w, r)
 	})
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprintf(w, "Hits: %d\n", cfg.fileserverHits)
+	cfg.mu.RLock() // lock for reading
+	defer cfg.mu.RUnlock() // ensure unlock after reading	
+
 	htmlTemplate := `
-    <html>
-    <body>
-        <h1>Welcome, Chirpy Admin</h1>
-        <p>Chirpy has been visited %d times!</p>
-    </body>
-    </html>	
+	<html>
+	<body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %d times!</p>
+	</body>
+	</html>	
 	`
 	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, htmlTemplate, cfg.fileserverHits)
 }
