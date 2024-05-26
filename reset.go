14fbcb68816dd576ecefe14edace4408fd18cc8b
Salomon Chambi <salomonchambi@gmail.com>
2024-05-26 15:23:41 -0500

    refactor: add mutex locks to prevent race conditions

--- reset.go
+++ reset.go
@@ -5,5 +5,11 @@ import (
 )
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.mu.RLock() // lock for writing
+	defer cfg.mu.RUnlock() // ensure unlock after writing
+
 	cfg.fileserverHits = 0
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Server hits have been reset to 0\n"))
 }
