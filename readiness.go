36b7ff9c85c46b9848b673d031084dfbb46c9e3b
Salomon Chambi <salomonchambi@gmail.com>
2024-05-25 22:45:07 -0500

    feat: create handler for healthz (readiness) endpoint

---
+++ readiness.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"net/http"
+)
+
+// Handler function for /healthz
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK\n"))
+}
