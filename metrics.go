89ff6d161aacbdbf4aa09d8f922a5390f8eda83e
Salomon Chambi <salomonchambi@gmail.com>
2024-05-27 15:51:40 -0500

    feat: adjust indentation on html template

--- metrics.go
+++ metrics.go
@@ -22,10 +22,10 @@ func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 
 	htmlTemplate := `
 	<html>
-	<body>
-		<h1>Welcome, Chirpy Admin</h1>
-		<p>Chirpy has been visited %d times!</p>
-	</body>
+		<body>
+			<h1>Welcome, Chirpy Admin</h1>
+			<p>Chirpy has been visited %d times!</p>
+		</body>
 	</html>	
 	`
 	w.Header().Set("Content-Type", "text/html")
