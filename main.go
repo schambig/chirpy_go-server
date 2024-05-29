bd6a7c89b69ec2aa3a628b9da0a8a22ad21bf653
Salomon Chambi <salomonchambi@gmail.com>
2024-05-28 22:21:45 -0500

    refactor: apply separation of concerns principle to validate_chirp

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 	"sync"
 	"log"
@@ -11,41 +10,6 @@ import (
 type apiConfig struct {
 	fileserverHits int
 	mu sync.RWMutex // only need one RWMutex to handle both reads and writes
-}
-
-// struct for the json body to expect
-type validChirp struct {
-	Body string `json:"body"`
-}
-
-func handlerValidChirp(w http.ResponseWriter, r *http.Request) {
-	var chirp validChirp
-
-	// decode the json request body into the chirp variable
-	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(&chirp)
-
-	if err != nil {
-		// respond with error if json decoding fails
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(map[string]string{"error":"Something went wrong when decoding JSON"})
-		return
-	}
-
-	// check length of the chirp (Body field)
-	if len(chirp.Body) > 140 {
-		// respond with error if Body field exceeds length
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(map[string]string{"error":"Chirp is too long"})
-		return		
-	}
-	
-	// respond with successful message if all went as expected
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]bool{"valid":true})
 }
 
 func main() {
