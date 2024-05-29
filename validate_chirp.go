003978ff95b54b9b7f83b50d916a5b79e7527df4
Salomon Chambi <salomonchambi@gmail.com>
2024-05-29 00:40:38 -0500

    refactor: apply separation of concerns principle to responses

--- validate_chirp.go
+++ validate_chirp.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"net/http"
-	"log"
 )
 
 // struct for the json body to expect
@@ -33,31 +32,4 @@ func handlerValidChirp(w http.ResponseWriter, r *http.Request) {
 	
 	// respond with successful message if all went as expected
 	respondWithJSON(w, http.StatusOK, map[string]bool{"valid":true})
-}
-
-func respondWithError(w http.ResponseWriter, code int, msg string) {
-	if code >= 500 {
-		log.Printf("Server error 5XX: %s,", msg)
-	} else if code >= 400 {
-		log.Printf("Client error 4XX: %s,", msg)
-	}
-	type errorResponse struct {
-		Error string `json:"error"`
-	}
-	respondWithJSON(w, code, errorResponse{
-		Error: msg,
-	})
-}
-	
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	dat, err := json.Marshal(payload)
-	if err != nil {
-		log.Printf("Error while marshalling JSON: %s,", err)
-		w.WriteHeader(500) // marshalling error is a server error
-		return
-	}
-	log.Printf("Successfully marshalled!")
-	w.WriteHeader(code)
-	w.Write(dat)
 }
