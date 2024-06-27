2509b96a7dfe2c59dc23cd3b22ee8b05f3d4645a
Salomon Chambi <salomonchambi@gmail.com>
2024-06-27 09:00:00 -0500

    feat: create users and save them to database

---
+++ handler_create_users.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type User struct {
+	ID int `json:"id"`
+	Email string `json:"email"`
+}
+
+func (cfg *apiConfig) handlerCreateUsers(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode request body")
+		return		
+	}
+
+	user, err := cfg.DB.CreateUser(params.Email)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create user")
+		return		
+	}
+
+	respondWithJSON(w, http.StatusCreated, User{
+		ID: user.ID,
+		Email: user.Email,
+	})
+}
