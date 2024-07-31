34bb89dd08b7ab6c1680dc102a6f07e9b8170500
Salomon Chambi <salomonchambi@gmail.com>
2024-07-31 09:00:00 -0500

    refactor: implement token extraction from r.Header in auth package

--- handler_update_users.go
+++ handler_update_users.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"net/http"
-	"strings"
 	"strconv"
 
 	"github.com/schambig/chirpy_go-server/internal/auth"
@@ -20,29 +19,19 @@ func (cfg *apiConfig) handlerUpdateUsers(w http.ResponseWriter, r *http.Request)
 		User
 	}
 
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't find JWT in r.Header")
+		return	
+	}
+
 	decoder := 	json.NewDecoder(r.Body)
 	params := parameters{}
-	err := decoder.Decode(&params)
+	err = decoder.Decode(&params)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't decode request body")
 		return			
 	}
-
-	// header format: Authorization: Bearer <token>, we just need the token
-	// If there are no values associated with the key, Get returns ""
-	authHeader := r.Header.Get("Authorization")
-	if authHeader == "" {
-		respondWithError(w, http.StatusUnauthorized, "No Authorization header provided")
-		return
-	}
-
-	const bearerPrefix = "Bearer "
-	if !strings.HasPrefix(authHeader, bearerPrefix) {
-		respondWithError(w, http.StatusUnauthorized, "Invalid token format in AuthZ header")
-		return
-	}
-
-	tokenStr := strings.TrimPrefix(authHeader, bearerPrefix)
 
 	// validate the signature of the JWT and extract the claims
 	token, err := jwt.ParseWithClaims(tokenStr, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
